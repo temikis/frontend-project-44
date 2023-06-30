4e116cc34451e783e2fcf295bebbaa50110cb2f0
temikis <temikis@mail.ru>
2023-07-01 01:18:59 +0200

    add Eslint

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,9 @@
 import readlineSync from 'readline-sync';
 
 const greet = () => {
-    console.log('Welcome to the Brain Games!');
-    const NAME_PLAYER = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${NAME_PLAYER}!`); 
+  console.log('Welcome to the Brain Games!');
+  const NAME_PLAYER = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${NAME_PLAYER}!`);
 };
 
 export default greet;
