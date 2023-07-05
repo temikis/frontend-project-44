7dd56b2a0fb1277e062b56ffcaaf71d8608fc6dd
temikis <temikis@mail.ru>
2023-07-05 22:34:05 +0200

    add brain-gdc

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -9,29 +9,29 @@ let countCorrectAnswer = 0;
 const COUNT_ROUND = 3;
 const MAX_NUMBER = 100;
 
-const calc = (number1, number2, operation) => {
-  let solution;
-  if (operation === 0) {
-    solution = number1 + number2;
-  } else if (operation === 1) {
-    solution = number1 - number2;
-  } else if (operation === 2) {
-    solution = number1 * number2;
+const gcd = (number1, number2) => {
+  let solution = 1;
+  const halfOfMin = Math.min(number1, number2) / 2;
+
+  for (let i = 2; i <= halfOfMin; i += 1) {
+    if (number1 % i === 0 && number2 % i === 0) {
+      solution = i;
+    }
   }
   return solution;
 };
 
 const getRandomIntInclusive = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
-console.log('What is the result of the expression?');
+console.log('Find the greatest common divisor of given numbers.');
 while (countCorrectAnswer < COUNT_ROUND) {
   const randomNumber1 = getRandomIntInclusive(1, MAX_NUMBER);
   const randomNumber2 = getRandomIntInclusive(1, MAX_NUMBER);
-  const randomOperation = getRandomIntInclusive(0, 2);
-  const operation = ['+', '-', '*'];
+  // const randomOperation = getRandomIntInclusive(0, 2);
+  // const operation = ['+', '-', '*'];
 
-  const equal = calc(randomNumber1, randomNumber2, randomOperation);
-  console.log(`Question: ${randomNumber1} ${operation[randomOperation]} ${randomNumber2}`);
+  const equal = gcd(randomNumber1, randomNumber2);
+  console.log(`Question: ${randomNumber1} ${randomNumber2}`);
   const answerPlayer = readlineSync.question('Your answer: ');
   if (equal === parseInt(answerPlayer, 10)) {
     console.log('Correct!');
