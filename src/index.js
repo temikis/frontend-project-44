30fed49268a14e07bdef2f9615344caebc67e3e2
temikis <temikis@mail.ru>
2023-07-18 20:33:35 +0200

    Fixed issues №7-9

--- src/index.js
+++ src/index.js
@@ -3,27 +3,20 @@ import readlineSync from 'readline-sync';
 const ROUNDS_COUNT = 3;
 
 export default (description, getRound) => {
-  let countCorrectAnswer = 0;
-
   console.log('Welcome to the Brain Games!');
   const namePlayer = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${namePlayer}!`);
   console.log(description);
-  while (countCorrectAnswer < ROUNDS_COUNT) {
+  for (let countCorrectAnswer = 0; countCorrectAnswer < ROUNDS_COUNT; countCorrectAnswer += 1) {
     const [correctAnswer, taskText] = getRound();
     console.log(`Question: ${taskText}`);
     const answerPlayer = readlineSync.question('Your answer: ');
-    if (correctAnswer === answerPlayer) {
-      console.log('Correct!');
-      countCorrectAnswer += 1;
-    } else {
-      console.log(
-        `'${answerPlayer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${namePlayer}!`,
-      );
-      break;
+    if (correctAnswer !== answerPlayer) {
+      console.log(`'${answerPlayer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${namePlayer}!`);
+      return;
     }
+    console.log('Correct!');
   }
-  if (countCorrectAnswer === 3) {
-    console.log(`Congratulations, ${namePlayer}!`);
-  }
+  console.log(`Congratulations, ${namePlayer}!`);
 };
