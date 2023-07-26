d0e5453b141ed53f29524edf5f705991c33af140
temikis <temikis@mail.ru>
2023-07-26 12:11:02 +0200

    Fixed issues №12: fix brain-even.js

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -5,19 +5,16 @@ const MIN_NUMBER = 1;
 const MAX_NUMBER = 100;
 const DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".';
 
+const isEver = (number) => number % 2 === 0;
+
+const getRound = () => {
+  const number = getRandomNumber(MIN_NUMBER, MAX_NUMBER);
+  const taskText = String(number);
+  const correctAnswer = isEver(number) ? 'yes' : 'no';
+
+  return [correctAnswer, taskText];
+};
+
 export default () => {
-  const getRound = () => {
-    let correctAnswer = '';
-    const number = getRandomNumber(MIN_NUMBER, MAX_NUMBER);
-    const taskText = `${number}`;
-    if (number % 2 === 0) {
-      correctAnswer = 'yes';
-    } else {
-      correctAnswer = 'no';
-    }
-
-    return [correctAnswer, taskText];
-  };
-
   runGame(DESCRIPTION, getRound);
 };
