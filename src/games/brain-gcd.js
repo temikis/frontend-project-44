6a4c678913c5ce075d40c1696990678e5dab3280
temikis <temikis@mail.ru>
2023-07-26 13:04:10 +0200

    fix brain-gcd.js and add new paths in Makefile

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -5,23 +5,27 @@ const MIN_NUMBER = 1;
 const MAX_NUMBER = 100;
 const DESCRIPTION = 'Find the greatest common divisor of given numbers.';
 
-export default () => {
-  const getRound = () => {
-    let correctAnswer = 1;
-    const number1 = getRandomNumber(MIN_NUMBER, MAX_NUMBER);
-    const number2 = getRandomNumber(MIN_NUMBER, MAX_NUMBER);
-    const taskText = `${number1} ${number2}`;
-    const min = Math.min(number1, number2);
-
-    for (let i = 2; i <= min; i += 1) {
-      if (number1 % i === 0 && number2 % i === 0) {
-        correctAnswer = i;
-      }
+const findGcd = (number1, number2) => {
+  const min = Math.min(number1, number2);
+  let solution = 1;
+  for (let i = 2; i <= min; i += 1) {
+    if (number1 % i === 0 && number2 % i === 0) {
+      solution = i;
     }
-    correctAnswer = correctAnswer.toString();
+  }
 
-    return [correctAnswer, taskText];
-  };
+  return solution;
+};
 
+const getRound = () => {
+  const number1 = getRandomNumber(MIN_NUMBER, MAX_NUMBER);
+  const number2 = getRandomNumber(MIN_NUMBER, MAX_NUMBER);
+  const taskText = `${number1} ${number2}`;
+  const correctAnswer = String(findGcd(number1, number2));
+
+  return [correctAnswer, taskText];
+};
+
+export default () => {
   runGame(DESCRIPTION, getRound);
 };
