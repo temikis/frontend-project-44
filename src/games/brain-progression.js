4c035c15ffcbbc6f441954785a5dcc056990e4b1
temikis <temikis@mail.ru>
2023-07-26 17:09:28 +0200

    fix brain-progression

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -6,24 +6,29 @@ const MAX_FIRST_NUMBER = 25;
 const DESCRIPTION = 'What number is missing in the progression?';
 const MIN_STEP = 1;
 const MAX_STEP = 10;
-const VALUES = 10;
+const VALUES_COUNT = 10;
+
+const getNumbersForTask = (firstNumber, step, valuesCount) => {
+  const numbers = [];
+  for (let i = 0; i < valuesCount; i += 1) {
+    numbers.push(firstNumber + step * i);
+  }
+
+  return numbers;
+};
+
+const getRound = () => {
+  const firstNumber = getRandomNumber(MIN_FIRST_NUMBER, MAX_FIRST_NUMBER);
+  const step = getRandomNumber(MIN_STEP, MAX_STEP);
+  const randomIndex = getRandomNumber(0, VALUES_COUNT - 1);
+  const numbers = getNumbersForTask(firstNumber, step, VALUES_COUNT);
+  const correctAnswer = String(numbers[randomIndex]);
+  numbers[randomIndex] = '..';
+  const taskText = numbers.join(' ');
+
+  return [correctAnswer, taskText];
+};
 
 export default () => {
-  const getRound = () => {
-    const firstNumber = getRandomNumber(MIN_FIRST_NUMBER, MAX_FIRST_NUMBER);
-    const step = getRandomNumber(MIN_STEP, MAX_STEP);
-    const randomIndex = getRandomNumber(0, VALUES - 1);
-    const numbers = [];
-
-    for (let i = 0; i < VALUES; i += 1) {
-      numbers.push(firstNumber + step * i);
-    }
-    const correctAnswer = numbers[randomIndex].toString();
-    numbers[randomIndex] = '..';
-    const taskText = numbers.join(' ');
-
-    return [correctAnswer, taskText];
-  };
-
   runGame(DESCRIPTION, getRound);
 };
