384fd2b04632010c92e40ea61858e153851bc2bb
temikis <temikis@mail.ru>
2023-07-31 14:29:25 +0200

    Fixed issues №13: add const limit

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -9,7 +9,8 @@ const isPrime = (number) => {
   if (number <= 1) {
     return false;
   }
-  for (let i = 2; i <= number / 2; i += 1) {
+  const limit = Math.sqrt(number);
+  for (let i = 2; i <= limit; i += 1) {
     if (number % i === 0) {
       return false;
     }
